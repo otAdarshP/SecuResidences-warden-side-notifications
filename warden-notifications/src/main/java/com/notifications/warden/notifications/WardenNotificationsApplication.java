32369c462292fec468513203f2748a1a564b58cc
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-05-26 19:11:58 +0530

    Initial commit

---
+++ warden-notifications/src/main/java/com/notifications/warden/notifications/WardenNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.notifications.warden.notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardenNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WardenNotificationsApplication.class, args);
+	}
+
+}
---
+++ warden-notifications/src/test/java/com/notifications/warden/notifications/WardenNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notifications.warden.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WardenNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
